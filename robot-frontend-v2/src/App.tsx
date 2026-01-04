dbe59f87c03978afec7b8e6211ef1ee7b474e9ce
Brennan Giroux <jbrgiroux@gmail.com>
2026-01-04 10:13:00 -0700

    Front end work

--- robot-frontend-v2/src/App.tsx
+++ robot-frontend-v2/src/App.tsx
@@ -1,6 +1,7 @@
 import { createSignal, onMount, onCleanup } from 'solid-js'
 import './App.css'
 import { startTelemetrySocket } from './stores/telemetry/telemetrySocket'
+import MonitorApplet from './applets/monitorApplet';
 
 
 function App() {
@@ -17,8 +18,14 @@ function App() {
           <h1 class={"text-5xl font-bold mb-4 text-white p-4"}>Robotic Arm Configuration</h1>
         </div>
         <div class={"flex h-11/12"}>
-          <div class={"w-50 bg-gray-200 p-4"}>
-            
+          <div class={"hamburger-item flex flex-col w-50 bg-gray-200 p-4 "}>
+           <h1>Monitor</h1>
+           <h1>Control</h1>
+           <h1>Configure</h1>
+           <h1>Update</h1> 
+          </div>
+          <div class={"w-full bg-white p-4 flex"}>
+            <MonitorApplet />
           </div>
         </div>
       </div>
---
+++ robot-frontend-v2/src/applets/monitorApplet.tsx
@@ -0,0 +1,9 @@
+function MonitorApplet() {
+  return (
+    <div class={"text-2xl"}>
+      Monitor Applet
+    </div>
+  )
+}
+
+export default MonitorApplet;
